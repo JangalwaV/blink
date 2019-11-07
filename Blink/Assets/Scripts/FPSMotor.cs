d1839d83ab7068863db2c8ca4170d97894310ad3
JangalwaV <57194134+JangalwaV@users.noreply.github.com>
2019-11-07 12:47:24 -0600

    Applying Basic Movement/Camera Control

---
+++ Blink/Assets/Scripts/FPSMotor.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(PlayerController))]
+
+public class FPSMotor : MonoBehaviour
+{
+    [SerializeField] Camera _camera = null;
+    [SerializeField] float _cameraAngleLimit = 70f;
+    //tracking camera angle to avoid weird conversions
+    private float _currentCameraRotationX = 0;
+
+    Rigidbody _rigidbody = null;
+
+    //store temp variables to calculate movement/rotation
+    Vector3 _movementThisFrame = Vector3.zero;
+    float _turnAmountThisFrame = 0;
+    float _lookAmountThisFrame = 0;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementThisFrame);
+        ApplyTurn(_turnAmountThisFrame);
+        ApplyLook(_lookAmountThisFrame);
+    }
+    public void Move(Vector3 requestedMovement)
+    {
+        //store movement for next FixedUpdate tick
+        _movementThisFrame = requestedMovement;
+    }
+
+    public void Turn(float turnAmount)
+    {
+        //store rotation for next FixedUpdate tick
+        _turnAmountThisFrame = turnAmount;
+    }
+
+    public void Look(float lookAmount)
+    {
+        //store rotation for next FixedUpdate tick
+        _lookAmountThisFrame = lookAmount;
+    }
+
+    public void Jump(float jumpForce)
+    {
+        _rigidbody.AddForce(Vector3.up * jumpForce);
+    }
+
+    void ApplyMovement(Vector3 moveVector)
+    {
+        //confirm we have movement, exit early if not
+        if (moveVector == Vector3.zero)
+            return;
+        //move rigidbody
+        _rigidbody.MovePosition(_rigidbody.position + moveVector);
+        //clear out movement until we get a new request
+        _movementThisFrame = Vector3.zero;
+    }
+
+    void ApplyTurn(float rotateAmount)
+    {
+        //confirm we have rotation, exit early if not
+        if (rotateAmount == 0)
+            return;
+        //rotate the body, covnert x,y,z to quaternion
+        Quaternion newRotation = Quaternion.Euler(0, rotateAmount, 0);
+        _rigidbody.MoveRotation(_rigidbody.rotation * newRotation);
+        //clear out turn until new request
+        _turnAmountThisFrame = 0;
+    }
+
+    void ApplyLook(float lookAmount)
+    {
+        //confirm we have a look, exit early if not
+        if (lookAmount == 0)
+            return;
+        //calculate and clamp new rotation before apply
+        _currentCameraRotationX -= lookAmount;
+        _currentCameraRotationX = Mathf.Clamp(_currentCameraRotationX, -_cameraAngleLimit, _cameraAngleLimit);
+        _camera.transform.localEulerAngles = new Vector3(_currentCameraRotationX, 0, 0);
+        //clear out x movement until new request
+        _lookAmountThisFrame = 0;
+    }
+}
--- Blink/Assets/Scripts/PlayerController.cs
+++ Blink/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(InputManager))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    InputManager _input = null;
+    FPSMotor _motor = null;
+
+    [SerializeField] float _moveSpeed = .1f;
+    [SerializeField] float _turnSpeed = 6f;
+    [SerializeField] float _jumpStrength = 10f;
+
+    private void Awake()
     {
-        
+        _input = GetComponent<InputManager>();
+        _motor = GetComponent<FPSMotor>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        //subscribes methods to input detection from InputManager
+        _input.MoveInput += OnMove;
+        _input.RotateInput += OnRotate;
+        _input.JumpInput += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        //unsubscribes methods from input detection from InputManager
+        _input.MoveInput -= OnMove;
+        _input.RotateInput -= OnRotate;
+        _input.JumpInput -= OnJump;
+    }
+
+    void OnMove(Vector3 movement)
+    {
+        //run Move in Motor with movespeed defined in PlayerController
+        _motor.Move(movement * _moveSpeed);
+    }
+
+    void OnRotate(Vector3 rotation)
+    {
+        //change camera/turn from Motor, parameters in PlayerController
+        _motor.Turn(rotation.y * _turnSpeed);
+        _motor.Look(rotation.x * _turnSpeed);
+    }
+
+    void OnJump()
+    {
+        //run Jump in Motor, parameters in PlayerController
+        _motor.Jump(_jumpStrength);
     }
 }
