9a34c590454ed5026318a705b0e66f883569d19b
JangalwaV <57194134+JangalwaV@users.noreply.github.com>
2019-11-07 10:05:44 -0600

    Adding Input Manager

---
+++ Blink/Assets/Scripts/InputManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] bool _invertVertical = false;
+
+    public event Action<Vector3> MoveInput = delegate { };      //Uses a Vector3 to find which direction to move
+    public event Action<Vector3> RotateInput = delegate { };    //uses a Vector3 to find which direction to rotate
+    public event Action JumpInput = delegate { };
+
+
+    void Update()
+    {
+        DetectMoveInput();
+        DetectRotateInput();
+        DetectJumpInput();
+    }
+
+    void DetectJumpInput()
+    {
+        //check for Spacebar press
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpInput?.Invoke();
+        }
+    }
+
+    void DetectRotateInput()
+    {
+        //get input from controller as a 0 or 1
+        float xInput = Input.GetAxisRaw("Mouse X");
+        float yInput = Input.GetAxisRaw("Mouse Y");
+
+        //Parse input
+        if (xInput != 0 || yInput != 0)
+        {
+            //check for inverted camera
+            if (_invertVertical)
+            {
+                yInput = -yInput;
+            }
+
+            //combine into a single vector
+            Vector3 rotation = new Vector3(yInput, xInput, 0);
+
+            //notify rotate
+            RotateInput?.Invoke(rotation);
+        }
+    }
+
+    void DetectMoveInput()
+    {
+        //get input from controller as a 0 or 1
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        //Parse input
+        if (xInput != 0 || yInput != 0)
+        {
+            //convert to local direction
+            Vector3 _horizontalMovement = transform.right * xInput;
+            Vector3 _forwardMovement = transform.forward * yInput;
+
+            //combine into a single vector
+            Vector3 movement = (_horizontalMovement + _forwardMovement).normalized;
+
+            //notify move
+            MoveInput?.Invoke(movement);
+        }
+    }
+}
